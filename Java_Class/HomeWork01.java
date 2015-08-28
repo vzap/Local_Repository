b126bf39c37aade5ed1438abb0b7c06592b5ee4a
vzap <m3dakar@gmail.com>
2015-08-28 17:28:31 -0500

    Java_homework
    Finished home work 01 for java

--- Java_Class/HomeWork01.java
+++ Java_Class/HomeWork01.java
@@ -1,7 +1,23 @@
-
 /**	Name:	Vincent Paz
 *	Date:	8/28/2015
 *	Class:	Programming Fund 1336
 *	Work:	Home Work 01
 */
 
+import static java.lang.System.out;
+
+public class HomeWork01 {
+	public static void main (String[] arg){
+		printstr();
+	}
+	public static void printstr(){
+		out.println("vvvv       vvvv       AAAAAAA          PPPPPPPPP");
+		out.println(" vvvv     vvvv       AAAA AAAA         PPPP   PPP");
+		out.println("  vvvv   vvvv       AAAA   AAAA        PPPP    PPP");
+		out.println("   vvvv vvvv       AAAA     AAAA       PPPP   PPP");
+		out.println("    vvvvvvv       AAAAAAAAAAAAAAA      PPPPPPPPP");
+		out.println("     vvvvv       AAAA        AAAAA     PPPP");
+		out.println("      vvv       AAAA          AAAAA    PPPP");
+		out.println("       v       AAAA            AAAAA   PPPP");
+	}
+}
